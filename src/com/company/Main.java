281cb4b5c9af7382e7ffd6c472f9084cc5b5a9be
robertson.sean <searob@icloud.com>
2018-02-13 09:28:37 -0800

    Implemented movment from room to room. Added lots of goodies to the room class.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,29 +4,47 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
-        HashMap<String, Room> RoomList = new HashMap<String, Room>();
-        RoomList.put("garage",new Room("garage", "The garage is full of cardboard boxes."));
-        RoomList.put("arcade",new Room("arcade", "The arcade is bright, loud, and full of games."));
-        RoomList.put("cave",new Room("cave","The cave is dark and damp"));
+        Room curRoom = addRooms();
+
         Scanner reader = new Scanner(System.in);
-        System.out.println("You have entered a room.");
+        System.out.println(curRoom.getDescription());
         String prompt = reader.next();
+
         while(true) {
-            if(RoomList.containsKey(prompt)){
-                System.out.println(RoomList.get(prompt).getDescription());
+            if(curRoom.hasRoom(prompt)){
+                curRoom=curRoom.getRoom(prompt);
+                System.out.println(curRoom.getDescription());
             }
            else if (prompt.equals("look")) {
 
-                System.out.println("The room is dark");
+                System.out.println(curRoom.getDescription());
             }
-            else if (prompt.equals("quit")) {
-                break;
-            }
+            else if (prompt.equals("quit")) break;
             else{
-                System.out.println("I'm sorry, I don't understand. Please try again: ");
+                System.out.println("You can't do that ");
             }
             prompt = reader.next();
         }
+    }
+    //This is a utility method to set up all the rooms and their connections.
+    //Returns the main Room.
+    private static Room addRooms() {
+        Room home = new Room("home", "You are in a simple gray room.");
+        Room cave = new Room("cave", "This room glitters with jewels.");
+        Room arcade = new Room("arcade","This  room is full of skee ball courts");
+        Room garage = new Room("garage","this room is full of cardboard boxes");
+        arcade.addRoom(home);
+        home.addRoom(garage);
+        garage.addRoom(home);
+        garage.addRoom(arcade);
+        arcade.addRoom(cave);
+        cave.addRoom(garage);
+        home.addRoom(cave);
+        home.addRoom(arcade);
+        cave.addRoom(home);
+        return home;
+
     }
 }
--- src/com/company/Room.java
+++ src/com/company/Room.java
@@ -1,14 +1,29 @@
 package com.company;
+import java.util.HashMap;
 
 public class Room {
     private String name;
     private String description;
+
+    HashMap<String, Room> connectedRooms = new HashMap<String, Room>();
+
     public Room(String name, String description) {
         this.name = name;
         this.description = description;
-
     }
 
+    public boolean hasRoom(String name) {
+        return connectedRooms.containsKey(name);
+    }
+
+    public Room getRoom(String name) {
+        return connectedRooms.get(name);
+    }
+
+    //This method gets a Room, then uses its name to add it to the list of connected rooms.
+    public void addRoom(Room room) {
+        connectedRooms.put(room.getName(),room);
+    }
     public String getName() {
         return name;
     }
@@ -24,4 +39,6 @@ public class Room {
     public void setDescription(String description) {
         this.description = description;
     }
+
+
 }
