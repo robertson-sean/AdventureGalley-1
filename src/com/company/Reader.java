54688b5adfa35cd36c88541f573ff1e1b2d88205
noahedmonds <edmonds.noah@vashonsd.org>
2018-03-02 08:51:26 -0800

    Merge branch 'master' of https://github.com/nedmonds/AdventureGalley
    # Conflicts:
    #	src/com/company/Main.java

--- src/com/company/Reader.java
+++ src/com/company/Reader.java
@@ -1,4 +1,5 @@
 package com.company;
+import javax.xml.bind.SchemaOutputResolver;
 import java.util.Scanner;
 
 public class Reader {
@@ -13,6 +14,7 @@ public class Reader {
     }
     public String readLine(String prompt){
         System.out.print(prompt);
+        System.out.println("");
         String str = input.nextLine();
         return str;
     }
