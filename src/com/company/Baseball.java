0f9ed69aed657232456d14356b94b6c13269dc45
profit.samuel <profit.samuel@vashonsd.org>
2018-03-15 10:50:03 -0700

    Merge branch 'master' of C:\Users\profit.samuel\IdeaProjects\AdventureGalley with conflicts.

--- src/com/company/Baseball.java
+++ src/com/company/Baseball.java
@@ -3,6 +3,6 @@ package com.company;
 public class Baseball extends Item {
 
     Baseball() {
-        super("baseball", "It is a pearly white baseball.", false);
+        super("baseball", "It is a pearly white baseball.", false, true);
     }
 }
