173812bb90dd180e3d171dc9261c9eb40c9dbb21
Andy James <ajames@vashonsd.org>
2018-03-16 08:22:10 -0700

    Fixed the main code.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,32 +9,35 @@ public class Main {
 
         Scanner reader = new Scanner(System.in);
         Reader input = new Reader();
-        String username = input.readLine("What is your name?");
+//        String username = input.readLine("What is your name?");
         System.out.println(curRoom.getDescription());
+
         //prints out items in starter room for the player to see
         System.out.println("In this room, there is:");
         System.out.println(curRoom.listItemsInRoom());
+
         // offer help, since this is the player's first time playing
-        input.readLine(curRoom.getDescription()+ " Type your command. If you need help, type 'help.'");
+        String prompt = input.readLine(curRoom.getDescription()+ " Type your command. If you need help, type 'help.'");
 
-        String prompt = reader.next();
-
-        String name = prompt;
-        Player user = new Player(name);
-        System.out.println(curRoom.getDescription());
-        prompt = reader.next();
 
         while(true) {
-            if(curRoom.hasRoom(prompt)) {
-                curRoom = curRoom.getRoom(prompt);
+            String[] words = prompt.split(" ");
+            String directObject = words[words.length-1];
+            String verb = words[0];
+            if(curRoom.hasItem(directObject)) {
+                Item whichItem = curRoom.getItem(directObject);
+                String response = whichItem.handle(verb);
+                System.out.println(response);
+            } else if(curRoom.hasRoom(verb)) {
+                curRoom = curRoom.getRoom(verb);
                 System.out.println(curRoom.getDescription());
                 System.out.print("In this room, there is:");
                 System.out.print(curRoom.listItemsInRoom());
             }
-            else if (prompt.equalsIgnoreCase("look")) {
+            else if (verb.equalsIgnoreCase("look")) {
                 System.out.println(curRoom.getDescription());
             }
-            else if (prompt.equalsIgnoreCase("quit")) {
+            else if (verb.equalsIgnoreCase("quit")) {
                 System.out.println("Are you sure you want to quit? (y/n)");
                 String quitPrompt = reader.next();
                 if(quitPrompt.equalsIgnoreCase("y")) {
@@ -45,13 +48,13 @@ public class Main {
                     System.out.println(curRoom.getDescription());
                 }
             }
-            else if (prompt.equalsIgnoreCase("help")){
+            else if (verb.equalsIgnoreCase("help")){
                 System.out.println("Type 'look' to explore your surroundings, Type 'quit' to exit the game, Type 'help' for help.");
             }
             else{
                 System.out.println("You can't do that ");
             }
-            prompt = reader.next();
+            prompt = reader.nextLine();
         }
     }
     //This is a utility method to set up all the rooms and their connections.
@@ -62,6 +65,9 @@ public class Main {
         Room arcade = new Room("arcade","This  room is full of skee ball courts");
         Room garage = new Room("garage","this room is full of cardboard boxes");
 
+        home.addItem(new Book());
+        home.addItem(new Apple());
+
         arcade.addRoom(home);
         arcade.addRoom(cave);
 
@@ -69,14 +75,14 @@ public class Main {
         home.addRoom(cave);
         home.addRoom(arcade);
 
-        home.itemsInRoom.addItem(new Banana());
-        home.itemsInRoom.addItem(new Item("note", "Welcome to our game.", false));
-        garage.itemsInRoom.addItem(new Item("Hammer", "A very heavy duty hammer.", false));
-        garage.itemsInRoom.addItem(new Item("Drill","Does not come with a drill bit.", false));
-        arcade.itemsInRoom.addItem(new Item("controller", "This is a video game controller or an unknown game system.", false));
-        arcade.itemsInRoom.addItem(new Item("jacket", " 80's style jacket with teal and purple detailing.", false));
-        cave.itemsInRoom.addItem(new Item("charcoal stick", " A stick of charcoal that looks like it has been on the walls", false));
-        cave.itemsInRoom.addItem(new Item("arrowhead", "An arrowhead make out of what seems to be some sort of bone", false));
+//        home.itemsInRoom.addItem(new Banana());
+//        home.itemsInRoom.addItem(new Item("note", "Welcome to our game.", false));
+//        garage.itemsInRoom.addItem(new Item("Hammer", "A very heavy duty hammer.", false));
+//        garage.itemsInRoom.addItem(new Item("Drill","Does not come with a drill bit.", false));
+//        arcade.itemsInRoom.addItem(new Item("controller", "This is a video game controller or an unknown game system.", false));
+//        arcade.itemsInRoom.addItem(new Item("jacket", " 80's style jacket with teal and purple detailing.", false));
+//        cave.itemsInRoom.addItem(new Item("charcoal stick", " A stick of charcoal that looks like it has been on the walls", false));
+//        cave.itemsInRoom.addItem(new Item("arrowhead", "An arrowhead make out of what seems to be some sort of bone", false));
 
         garage.addRoom(home);
         garage.addRoom(arcade);
